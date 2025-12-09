c096f922921243ae3105bdb3a9ed8505e2ea8994
ex_ouyt3 <ex_ouyt3@partner.midea.com>
2025-12-09 11:10:40 +0800

    加入监控

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.monitor.FiveMinMonitor;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        BinanceCombinedServer.main(args);
+        FiveMinMonitor.main(args);
+    }
+}
