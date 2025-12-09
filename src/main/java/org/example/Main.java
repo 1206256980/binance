c2bf671873a9864fd955c3622d385321dea6c390
ex_ouyt3 <ex_ouyt3@partner.midea.com>
2025-12-09 15:27:26 +0800

    测试

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.example.monitor.FiveMinMonitor;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        BinanceCombinedServer.main(args);
-        FiveMinMonitor.main(args);
+        BinanceCombinedServer.main(args);
+//        FiveMinMonitor.main(args);
     }
 }
